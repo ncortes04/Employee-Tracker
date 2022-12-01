7f21daba0dced81e795ce717fb7050f1860e5bae
ncortes04 <nickapoleon@gmail.com>
2022-11-30 18:11:59 -0800

    test

--- db/query.sql
+++ db/query.sql
@@ -3,3 +3,4 @@ FROM employee
 LEFT JOIN employee manager on manager.id = employee.manager_id
 INNER JOIN role ON (role.id = employee.role_id) INNER JOIN department ON (department.id = role.department_id) ORDER BY employee.id;
 
+/* daddada */
